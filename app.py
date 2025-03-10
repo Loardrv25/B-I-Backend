08317cc32ad3f72c7ebc063663c4d88dcbe808cd
Loardrv25 <rodrigo.vela.w@googlemail.com>
2025-03-10 21:48:08 +0100

    Corrigiendo Flask para Render (puerto dinámico)

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def get_tendencias():
     data = {"negocios": ["E-commerce de nicho", "Cursos online", "Vending Machines saludables"]}
     return jsonify(data)
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Render asigna el puerto automáticamente
+    app.run(host="0.0.0.0", port=port, debug=True)
+
+
