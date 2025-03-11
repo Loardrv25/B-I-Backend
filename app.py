726784b3f4a52808e0ed0eeedc0297b9d3b492f4
Loardrv25 <rodrigo.vela.w@googlemail.com>
2025-03-11 10:49:26 +0100

    Agregando ruta '/' para evitar error 404

--- app.py
+++ app.py
@@ -3,15 +3,18 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return "Backend funcionando correctamente 🚀"
+
 @app.route('/tendencias', methods=['GET'])
 def get_tendencias():
     data = {"negocios": ["E-commerce de nicho", "Cursos online", "Vending Machines saludables"]}
     return jsonify(data)
 
-import os
-
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))  # Render asigna el puerto automáticamente
+    import os
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port, debug=True)
 
 
