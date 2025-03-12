cf5e1d83b9f03ac77e2c90b8bdc360de379006fc
Loardrv25 <rodrigo.vela.w@googlemail.com>
2025-03-12 13:47:08 +0100

    Habilitando CORS en Flask para permitir acceso desde GitHub Pages

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-
 from flask import Flask, jsonify
+from flask_cors import CORS  # Importar CORS
 
 app = Flask(__name__)
+CORS(app)  # Habilitar CORS en toda la aplicación
 
 @app.route('/')
 def home():
@@ -9,12 +10,16 @@ def home():
 
 @app.route('/tendencias', methods=['GET'])
 def get_tendencias():
-    data = {"negocios": ["E-commerce de nicho", "Cursos online", "Vending Machines saludables"]}
+    data = {
+        "negocios": [
+            "E-commerce de nicho",
+            "Cursos online",
+            "Vending Machines saludables"
+        ]
+    }
     return jsonify(data)
 
 if __name__ == '__main__':
     import os
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 10000))  # Render usa este puerto
     app.run(host="0.0.0.0", port=port, debug=True)
-
-
